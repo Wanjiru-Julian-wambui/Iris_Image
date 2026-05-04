48c7ba3bc5f8d9325b188d74a196c06847cd4c06
Wanjiru-Julian-wambui <julianwambuiw@gmail.com>
2026-05-04 19:54:37 +0300

    configure public notes error

--- app/Http/Controllers/ImageNoteController.php
+++ app/Http/Controllers/ImageNoteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Image;
 use App\Models\ImageNote;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ImageNoteController extends Controller
 {
