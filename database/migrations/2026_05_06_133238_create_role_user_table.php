e130fac362773ebcfafd2e87d8329c6f87b74411
Wanjiru-Julian-wambui <julianwambuiw@gmail.com>
2026-05-07 16:45:59 +0300

    Remove duplicate role user table

--- database/migrations/2026_05_06_133238_create_role_user_table.php
+++ database/migrations/2026_05_06_133238_create_role_user_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('role_user')) {
+            Schema::create('role_user', function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+            });
+        }
     }
 
     /**
